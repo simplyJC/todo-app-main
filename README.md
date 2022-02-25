d35b5ded3461e67a701787f76978302890536bd4
jcaltamia <jayceealtamia@gmail.com>
2022-02-25 22:30:38 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,86 @@
+# Frontend Mentor - Todo app solution
+
+This is a solution to the [Todo app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/todo-app-Su1_KokOW). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Add new todos to the list
+- Mark todos as complete
+- Delete todos from the list
+- Filter by all/active/complete todos
+- Clear all completed todos
+- Toggle light and dark mode
+- **Bonus**: Drag and drop to reorder items on the list
+
+### Screenshot
+
+![](./screenshot.jpg)
+
+
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+- Vanilla Javascript
+- Sortable Jquery
+
+
+### What I learned
+
+This is the hardest challenge that I take in frontendmentor. I took me a long time to figure out how to code a Todo App in javascript with the help of some resources I found in the internet. Somehow I was able to make it. I know there are still a lot of modifications needed but this is by far the solution that I can do. 
+
+### Continued development
+
+I am still a beginner in Javascript. There is still a lot to learn. I am looking forward to take more Javascript challenges to further enhance my learning. 
+
+
+
+### Useful resources
+
+- [https://freshman.tech/todo-list/](https://freshman.tech/todo-list/) - This help me to better understand how adding and removing tasks. 
+- [How to Code A Better To-Do List - Tutorial](https://www.youtube.com/watch?v=W7FaYfuwu70&t=1121s) - This is an amazing tutorial from WebDev which showing how you can save a tasks in local storage. This is somehow different but I just studied it and learn how things worked. 
+- [Jquery Sortable](https://jqueryui.com/sortable/) - It will help you out in reordering tasks with a few javascript code. 
+
+- [How To Create Dark Theme Switcher For Website Using CSS and JS](https://www.youtube.com/watch?v=D1yg4T37qYo) - One of the best tutorial in changing Theme.
+
+- [Todo App FrontEndMentor by Quang Phan](https://www.frontendmentor.io/solutions/todo-app-Z3uvRFUqu)- I refer to this  Todo App on how he manage to reorder task. 
+
+
+
+## Author
+
+- Website - [SimplyJC](https://www.simplyjc.com)
+- Frontend Mentor - [@simplyJC](https://www.frontendmentor.io/profile/simplyJC)
+- Twitter - [@jcaltamia](https://twitter.com/jcaltamia)
+
---
+++ app/js/main.js
@@ -0,0 +1,173 @@
+/*******************************Change Theme*********************************/
+const toggle = document.querySelector('.todo__toggle');
+const theme = document.querySelector('[data-theme]');
+const toggleIcon = document.querySelector('.todo__toggle-image');
+
+toggle.addEventListener('click', () => {
+  if (theme.getAttribute('data-theme') === 'light') {
+    theme.setAttribute('data-theme', 'dark');
+    toggleIcon.src = '/images/icon-sun.svg';
+  } else {
+    theme.setAttribute('data-theme', 'light');
+    toggleIcon.src = '/images/icon-moon.svg';
+  }
+});
+//initialize theme
+function setTheme() {
+  const darkThemeMq = window.matchMedia('(prefers-color-scheme: dark)');
+  if (darkThemeMq.matches) {
+    // Theme set to dark.
+    theme.setAttribute('data-theme', 'dark');
+  } else {
+    // Theme set to light.
+  }
+}
+setTheme();
+
+/*******************************Todo App*********************************/
+
+const newTaskInput = document.querySelector('[data-new-task-input]');
+const newTaskForm = document.querySelector('[data-new-task-form]');
+const tasks = document.querySelector('.todo__tasks');
+//Creating KEY
+const LOCAL_STORAGE_LIST_KEY = 'task.lists';
+
+//check if there is local storage or set empty
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
+
+let itemLeftCount = document.querySelectorAll('[data-task-remaining]');
+let buttonCompleted = document.querySelectorAll('[data-clear-completed]');
+let buttonFilterComplete = document.querySelector('[data-completed-button]');
+let buttonFilterActive = document.querySelector('[data-active-button]');
+let buttonFilterAll = document.querySelector('[data-all-button]');
+
+//Task List Even Listener when user click each task i.e. delete and complete task
+tasks.addEventListener('click', (e) => {
+  //When user put check on checkbox
+  if (e.target.classList.contains('todo__task--complete')) {
+    let itemKey = e.target.parentElement.dataset.listId;
+    toggleDone(itemKey);
+  }
+  if (e.target.classList.contains('todo__task--delete')) {
+    //DELETE When user Click  X Button
+    let itemKey = e.target.parentElement.dataset.listId;
+    lists = lists.filter((list) => list.id !== parseInt(itemKey));
+    itemKey = null;
+    saveAndRender();
+  }
+});
+
+function toggleDone(key) {
+  let index = lists.findIndex((list) => list.id === Number(key));
+  lists[index].checked = !lists[index].checked;
+  saveAndRender();
+}
+
+//Add New Task
+newTaskForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const taskName = newTaskInput.value;
+  if (taskName === null || taskName === '') return;
+  const task = createTask(taskName);
+  newTaskInput.value = null;
+  lists.push(task);
+  saveAndRender();
+});
+
+function createTask(name) {
+  return {
+    id: Date.now(),
+    checked: false,
+    name: name,
+  };
+}
+
+function saveAndRender() {
+  save();
+  render(lists);
+}
+
+//save tasks in local storage
+function save() {
+  localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists));
+}
+
+function render(listsFilter) {
+  let emptyState = document.querySelector('.empty-state');
+  if (listsFilter.length === 0) {
+    emptyState.style.display = 'block';
+    itemLeftCount.forEach(
+      (itemLeft) => (itemLeft.innerText = 'No Tasks remaining')
+    );
+  } else {
+    emptyState.style.display = 'none';
+  }
+
+  clearTasks(tasks);
+  //loop task in a task Lists
+  listsFilter.forEach((list) => {
+    const item = document.createElement('div');
+    let isChecked = list.checked ? 'todo__done' : '';
+    item.innerHTML = ` 
+        <input id="${list.id}" class="todo__task--complete ${isChecked}" type="checkbox"/>
+        <label class="todo__task-name" for="${list.id}">${list.name}</label>
+        <button class="todo__task--delete">
+          <img src="/images/icon-cross.svg" alt="delete button " />
+        </button>
+    `;
+    item.classList.add('todo__task');
+    item.dataset.listId = list.id;
+    tasks.appendChild(item);
+    itemTaskCount();
+  });
+}
+//Count Task Left Function
+function itemTaskCount() {
+  const incompleteTaskCount = lists.filter((task) => !task.checked).length;
+  const taskString = incompleteTaskCount === 1 ? 'task' : 'tasks';
+  itemLeftCount.forEach(
+    (itemLeft) =>
+      (itemLeft.innerText = `${incompleteTaskCount} ${taskString} remaining`)
+  );
+}
+
+//Clear Completed Event Listener, Loop for each buttons one for mobile view and Desktop View
+buttonCompleted.forEach((button) =>
+  button.addEventListener('click', () => {
+    lists = lists.filter((list) => list.checked === false);
+    saveAndRender();
+  })
+);
+
+//Filter Completed Even Listener
+
+buttonFilterComplete.addEventListener('click', () => {
+  let filterComplete = lists.filter((task) => task.checked === true);
+  render(filterComplete);
+});
+
+//Filter Active
+buttonFilterActive.addEventListener('click', () => {
+  let filterActive = lists.filter((task) => !task.checked);
+  render(filterActive);
+});
+
+buttonFilterAll.addEventListener('click', () => {
+  render(lists);
+});
+
+//Clear task Function
+function clearTasks(element) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+}
+
+$(function () {
+  $('#sortable').sortable();
+});
+$(function () {
+  $('#sortable').sortable();
+});
+
+render(lists);
